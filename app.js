4dae10e34448655e3dedf9de2bed4ecc5af06f56
ritesh423 <riteshsingh.py@gmail.com>
2023-04-18 14:21:06 +0530

    Basic Template for Project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Campground = require("./models/campground.js");
+
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "Bhartiye camp",
+    description: "Bhartiyon ke liye camp",
+  });
+  await camp.save();
+  res.send(camp);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
