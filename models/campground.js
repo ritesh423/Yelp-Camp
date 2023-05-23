238294c025c469b12f2f1eba2e51e2df638d0d60
ritesh423 <riteshsingh.py@gmail.com>
2023-05-23 13:09:29 +0530

    Code Update / Buggy Code

--- models/campground.js
+++ models/campground.js
@@ -3,10 +3,26 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
   title: String,
-  imageUrl: String,
   price: Number,
   description: String,
+  image: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,11 @@
+// const env = require("dotenv").config();
+
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
-const axios = require("axios");
-const env = require("dotenv").config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
 const db = mongoose.connection;
 
@@ -22,39 +18,20 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 20; i++) {
-    // setup
-    const placeSeed = Math.floor(Math.random() * places.length);
-    const descriptorsSeed = Math.floor(Math.random() * descriptors.length);
-    const citySeed = Math.floor(Math.random() * cities.length);
-
-    // seed data into campground
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      imageUrl: await seedImg(),
-      title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
-      location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
       description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
+      price,
     });
-
     await camp.save();
   }
 };
-
-async function seedImg() {
-  try {
-    const resp = await axios.get("https://api.unsplash.com/photos/random", {
-      params: {
-        client_id: process.env.UNSPLASH_KEY,
-        collections: 1114848,
-      },
-    });
-    console.log("Fetched image URL:", imageUrl);
-    return resp.data.urls.small;
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 seedDB().then(() => {
   mongoose.connection.close();
